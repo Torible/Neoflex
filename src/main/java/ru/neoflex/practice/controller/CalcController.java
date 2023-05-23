0276af237795350590200ee1cc42893876195351
AfonkinIDK <AfonkinIDK@gmail.com>
2023-05-23 09:54:11 +0300

    H2 DB

--- src/main/java/ru/neoflex/practice/controller/CalcController.java
+++ src/main/java/ru/neoflex/practice/controller/CalcController.java
@@ -5,12 +5,21 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import javax.validation.Valid;
 
 @RestController
 public class CalcController {
+
+    @Autowired
+    private Repos repository;
+
     @Operation(summary = "Get sum of two numbers")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "There is a result",
@@ -18,7 +27,10 @@ public class CalcController {
     @GetMapping("/plus/{a}/{b}")
     int Plus(@PathVariable("a")int a, @PathVariable("b")int b)
     {
-        return a + b;
+        int result = a + b;
+        Calculation calculation = new Calculation(a, "+", b, result);
+        repository.save(calculation);
+        return result;
     }
 
     @Operation(summary = "Get diff of two numbers")
@@ -28,6 +40,44 @@ public class CalcController {
     @GetMapping("/minus/{a}/{b}")
     int Minus(@PathVariable("a")int a, @PathVariable("b")int b)
     {
-        return a - b;
+        int result = a - b;
+        Calculation calculation = new Calculation(a, "-", b, result);
+        repository.save(calculation);
+        return result;
+    }
+
+    @GetMapping("/calculations")
+    public List <Calculation> getAllCalculations() {
+        return repository.findAll();
+    }
+    @GetMapping("/calculations/{id}")
+    public ResponseEntity< Calculation > getCalculationById(@PathVariable(value = "id") Long calculationId)
+            throws ResourceNotFoundException {
+        Calculation calculation = repository.findById(calculationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Calculation not found for this id :: " + calculationId));
+        return ResponseEntity.ok().body(calculation);
+    }
+    @PutMapping("/calculations/{id}")
+    public ResponseEntity < Calculation > updateCalculation(@PathVariable(value = "id") Long calculationId,
+                                                            @Valid @RequestBody Calculation calculationDetails) throws ResourceNotFoundException {
+        Calculation calculation = repository.findById(calculationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Calculation not found for this id :: " + calculationId));
+
+        calculation.setOperation(calculationDetails.getOperation());
+        calculation.setA(calculationDetails.getA());
+        calculation.setB(calculationDetails.getB());
+        calculation.setResult(calculationDetails.getResult());
+        final Calculation updatedCalculation = repository.save(calculation);
+        return ResponseEntity.ok(updatedCalculation);
+    }
+    @DeleteMapping("/calculations/{id}")
+    public Map< String, Boolean > deleteCalculation(@PathVariable(value = "id") Long calculationId)
+            throws ResourceNotFoundException {
+        Calculation calculation = repository.findById(calculationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Calculation not found for this id :: " + calculationId));
+        repository.delete(calculation);
+        Map< String, Boolean > response = new HashMap< >();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
